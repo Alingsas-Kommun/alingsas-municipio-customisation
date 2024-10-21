8b48b6b1f7e99960ea234be7e05ab050cc6bf71f
Michael Claesson <michael.claesson@consid.se>
2024-10-21 22:02:43 +0200

    Bug fix for nesting of filters

--- includes/ExtraSettings.php
+++ includes/ExtraSettings.php
@@ -40,152 +40,144 @@ class ExtraSettings {
     }
 
     public function additional_settings() {
-        // General
-        add_action('acf/include_fields', function () {
-            if (! function_exists('acf_add_local_field_group')) {
-                return;
-            }
+        if (!function_exists('acf_add_local_field_group')) {
+            return;
+        }
 
-            acf_add_local_field_group(array(
-                'key' => 'group_671638b4358a9',
-                'title' => 'Utseende',
-                'fields' => array(
+        // General
+        acf_add_local_field_group(array(
+            'key' => 'group_671638b4358a9',
+            'title' => 'Utseende',
+            'fields' => array(
+                array(
+                    'key' => 'field_671638b487dbd',
+                    'label' => 'Bakgrundsfärg',
+                    'name' => 'background_color',
+                    'aria-label' => '',
+                    'type' => 'select',
+                    'instructions' => '',
+                    'required' => 0,
+                    'conditional_logic' => 0,
+                    'wrapper' => array(
+                        'width' => '',
+                        'class' => '',
+                        'id' => '',
+                    ),
+                    'choices' => array(
+                        'standard' => 'Standard',
+                        'gray' => 'Grå',
+                    ),
+                    'default_value' => 'standard',
+                    'return_format' => 'value',
+                    'multiple' => 0,
+                    'allow_custom' => 0,
+                    'placeholder' => '',
+                    'search_placeholder' => '',
+                    'allow_null' => 0,
+                    'ui' => 0,
+                    'ajax' => 0,
+                ),
+            ),
+            'location' => array(
+                array(
                     array(
-                        'key' => 'field_671638b487dbd',
-                        'label' => 'Bakgrundsfärg',
-                        'name' => 'background_color',
-                        'aria-label' => '',
-                        'type' => 'select',
-                        'instructions' => '',
-                        'required' => 0,
-                        'conditional_logic' => 0,
-                        'wrapper' => array(
-                            'width' => '',
-                            'class' => '',
-                            'id' => '',
-                        ),
-                        'choices' => array(
-                            'standard' => 'Standard',
-                            'gray' => 'Grå',
-                        ),
-                        'default_value' => 'standard',
-                        'return_format' => 'value',
-                        'multiple' => 0,
-                        'allow_custom' => 0,
-                        'placeholder' => '',
-                        'search_placeholder' => '',
-                        'allow_null' => 0,
-                        'ui' => 0,
-                        'ajax' => 0,
+                        'param' => 'post_type',
+                        'operator' => '==',
+                        'value' => 'mod-text',
                     ),
                 ),
-                'location' => array(
+                array(
                     array(
-                        array(
-                            'param' => 'post_type',
-                            'operator' => '==',
-                            'value' => 'mod-text',
-                        ),
-                    ),
-                    array(
-                        array(
-                            'param' => 'block',
-                            'operator' => '==',
-                            'value' => 'acf/text',
-                        ),
+                        'param' => 'block',
+                        'operator' => '==',
+                        'value' => 'acf/text',
                     ),
                 ),
-                'menu_order' => 0,
-                'position' => 'side',
-                'style' => 'default',
-                'label_placement' => 'left',
-                'instruction_placement' => 'label',
-                'hide_on_screen' => '',
-                'active' => true,
-                'description' => '',
-                'show_in_rest' => 0,
-                'acfe_display_title' => '',
-                'acfe_autosync' => array(
-                    0 => 'json',
-                ),
-                'acfe_form' => 0,
-                'acfe_meta' => '',
-                'acfe_note' => '',
-            ));
-        });
+            ),
+            'menu_order' => 0,
+            'position' => 'side',
+            'style' => 'default',
+            'label_placement' => 'left',
+            'instruction_placement' => 'label',
+            'hide_on_screen' => '',
+            'active' => true,
+            'description' => '',
+            'show_in_rest' => 0,
+            'acfe_display_title' => '',
+            'acfe_autosync' => array(
+                0 => 'json',
+            ),
+            'acfe_form' => 0,
+            'acfe_meta' => '',
+            'acfe_note' => '',
+        ));
 
         // Card
-        add_action('acf/include_fields', function () {
-            if (! function_exists('acf_add_local_field_group')) {
-                return;
-            }
-
-            acf_add_local_field_group(array(
-                'key' => 'group_67161fd718251',
-                'title' => 'Kortutseende',
-                'fields' => array(
+        acf_add_local_field_group(array(
+            'key' => 'group_67161fd718251',
+            'title' => 'Kortutseende',
+            'fields' => array(
+                array(
+                    'key' => 'field_67161fd750750',
+                    'label' => 'Färg på huvud',
+                    'name' => 'card_head_color',
+                    'aria-label' => '',
+                    'type' => 'select',
+                    'instructions' => '',
+                    'required' => 0,
+                    'conditional_logic' => 0,
+                    'wrapper' => array(
+                        'width' => '',
+                        'class' => '',
+                        'id' => '',
+                    ),
+                    'choices' => array(
+                        'standard' => 'Standard',
+                        'winered' => 'Vinröd',
+                    ),
+                    'default_value' => false,
+                    'return_format' => 'value',
+                    'multiple' => 0,
+                    'allow_custom' => 0,
+                    'placeholder' => '',
+                    'search_placeholder' => '',
+                    'allow_null' => 0,
+                    'ui' => 0,
+                    'ajax' => 0,
+                ),
+            ),
+            'location' => array(
+                array(
                     array(
-                        'key' => 'field_67161fd750750',
-                        'label' => 'Färg på huvud',
-                        'name' => 'card_head_color',
-                        'aria-label' => '',
-                        'type' => 'select',
-                        'instructions' => '',
-                        'required' => 0,
-                        'conditional_logic' => 0,
-                        'wrapper' => array(
-                            'width' => '',
-                            'class' => '',
-                            'id' => '',
-                        ),
-                        'choices' => array(
-                            'standard' => 'Standard',
-                            'winered' => 'Vinröd',
-                        ),
-                        'default_value' => false,
-                        'return_format' => 'value',
-                        'multiple' => 0,
-                        'allow_custom' => 0,
-                        'placeholder' => '',
-                        'search_placeholder' => '',
-                        'allow_null' => 0,
-                        'ui' => 0,
-                        'ajax' => 0,
+                        'param' => 'post_type',
+                        'operator' => '==',
+                        'value' => 'mod-manualinput',
                     ),
                 ),
-                'location' => array(
+                array(
                     array(
-                        array(
-                            'param' => 'post_type',
-                            'operator' => '==',
-                            'value' => 'mod-manualinput',
-                        ),
-                    ),
-                    array(
-                        array(
-                            'param' => 'block',
-                            'operator' => '==',
-                            'value' => 'acf/manualinput',
-                        ),
+                        'param' => 'block',
+                        'operator' => '==',
+                        'value' => 'acf/manualinput',
                     ),
                 ),
-                'menu_order' => 0,
-                'position' => 'side',
-                'style' => 'default',
-                'label_placement' => 'left',
-                'instruction_placement' => 'label',
-                'hide_on_screen' => '',
-                'active' => true,
-                'description' => '',
-                'show_in_rest' => 0,
-                'acfe_display_title' => '',
-                'acfe_autosync' => array(
-                    0 => 'json',
-                ),
-                'acfe_form' => 0,
-                'acfe_meta' => '',
-                'acfe_note' => '',
-            ));
-        });
+            ),
+            'menu_order' => 0,
+            'position' => 'side',
+            'style' => 'default',
+            'label_placement' => 'left',
+            'instruction_placement' => 'label',
+            'hide_on_screen' => '',
+            'active' => true,
+            'description' => '',
+            'show_in_rest' => 0,
+            'acfe_display_title' => '',
+            'acfe_autosync' => array(
+                0 => 'json',
+            ),
+            'acfe_form' => 0,
+            'acfe_meta' => '',
+            'acfe_note' => '',
+        ));
     }
 }

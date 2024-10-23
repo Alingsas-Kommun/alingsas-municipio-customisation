8af0dcb074899127794943d8b9494dc4859821f5
Michael Claesson <michael.claesson@consid.se>
2024-10-23 11:09:52 +0200

    Support for background color

--- includes/modules/Posts.php
+++ includes/modules/Posts.php
@@ -6,6 +6,31 @@ use AlingsasCustomisation\Plugin;
 
 class Posts {
     public function __construct() {
+        // Extra classes
+        add_filter('Modularity/Display/BeforeModule::classes', function ($classes, $args, $posttype, $ID) {
+            if ($posttype === 'mod-posts') {
+                $background_color = get_field('background_color', $ID);
+
+                if (!empty($background_color) && $background_color !== 'none') {
+                    $overlap = get_field('overlap_top_bottom', $ID);
+                    $tilted_bottom = get_field('tilted_bottom', $ID);
+
+                    $classes[] = 'modularity-mod--has-background-color';
+                    $classes[] = 'modularity-mod--background-color-' . $background_color;
+
+                    if ($overlap) {
+                        $classes[] = 'modularity-mod--background-overlap';
+                    }
+
+                    if ($tilted_bottom) {
+                        $classes[] = 'modularity-mod--background-tilted';
+                    }
+                }
+            }
+
+            return $classes;
+        }, 10, 4);
+
         // Add option to display post as events calendar
         add_action('acf/load_field/name=posts_display_as', function ($field) {
             if (class_exists('Tribe__Events__Main')) {

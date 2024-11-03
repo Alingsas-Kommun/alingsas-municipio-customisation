03e859c94713a6a88723e7c4339677d997cc7c70
Michael Claesson <michael.claesson@consid.se>
2024-11-03 18:00:29 +0100

    Versioning

--- includes/Styles.php
+++ includes/Styles.php
@@ -2,15 +2,17 @@
 
 namespace AlingsasCustomisation\Includes;
 
+use AlingsasCustomisation\Plugin;
+
 class Styles {
     public function __construct() {
         add_action('wp_enqueue_scripts', function () {
             $manifest_path = dirname(plugin_dir_path(__FILE__)) . '/dist/css/manifest.json';
             if (file_exists($manifest_path)) {
                 $manifest = json_decode(file_get_contents($manifest_path));
-                wp_enqueue_style('alingsas-style', dirname(plugin_dir_url(__FILE__)) . '/dist/css/' . $manifest->{'main.css'}, null);
+                wp_enqueue_style('alingsas-style', dirname(plugin_dir_url(__FILE__)) . '/dist/css/' . $manifest->{'main.css'}, null, Plugin::VERSION);
             } else {
-                wp_enqueue_style('alingsas-style', dirname(plugin_dir_url(__FILE__)) . '/dist/css/main.css', null);
+                wp_enqueue_style('alingsas-style', dirname(plugin_dir_url(__FILE__)) . '/dist/css/main.css', null, Plugin::VERSION);
             }
         });
     }

20653d248069837dd847032d5d634996ee61b146
Michael Claesson <michael.claesson@consid.se>
2024-11-19 14:27:40 +0000

    settings & formatting

--- helpers/Posts.php
+++ helpers/Posts.php
@@ -3,46 +3,46 @@
 namespace AlingsasCustomisation\Helpers;
 
 class Posts {
-	public static function getBreadcrumbs( \WP_Post $post, callable $filterSort ) {
-		if ( ! $post instanceof \WP_Post ) {
+	public static function getBreadcrumbs(\WP_Post $post, callable $filterSort) {
+		if (! $post instanceof \WP_Post) {
 			return [];
 		}
 
-		$postTypeObj = get_post_type_object( $post->post_type );
-		if ( $postTypeObj ) {
-			$breadcrumbs[] = [ 
+		$postTypeObj = get_post_type_object($post->post_type);
+		if ($postTypeObj) {
+			$breadcrumbs[] = [
 				'title' => $postTypeObj->labels->name,
-				'url'   => get_post_type_archive_link( $post->post_type )
+				'url'   => get_post_type_archive_link($post->post_type)
 			];
 		}
 
-		if ( $post->post_type === 'post' ) {
-			$categories = get_the_category( $post->ID );
-			if ( ! empty( $categories ) ) {
+		if ($post->post_type === 'post') {
+			$categories = get_the_category($post->ID);
+			if (! empty($categories)) {
 				$category      = $categories[0];
-				$breadcrumbs[] = [ 
+				$breadcrumbs[] = [
 					'title' => $category->name,
-					'url'   => get_category_link( $category->term_id )
+					'url'   => get_category_link($category->term_id)
 				];
 			}
 		}
 
-		if ( is_post_type_hierarchical( $post->post_type ) ) {
-			$ancestors = get_post_ancestors( $post->ID );
-			foreach ( array_reverse( $ancestors ) as $ancestor ) {
-				$breadcrumbs[] = [ 
-					'title' => get_the_title( $ancestor ),
-					'url'   => get_permalink( $ancestor )
+		if (is_post_type_hierarchical($post->post_type)) {
+			$ancestors = get_post_ancestors($post->ID);
+			foreach (array_reverse($ancestors) as $ancestor) {
+				$breadcrumbs[] = [
+					'title' => get_the_title($ancestor),
+					'url'   => get_permalink($ancestor)
 				];
 			}
 		}
 
-		$breadcrumbs[] = [ 
-			'title' => get_the_title( $post->ID ),
-			'url'   => get_permalink( $post->ID )
+		$breadcrumbs[] = [
+			'title' => get_the_title($post->ID),
+			'url'   => get_permalink($post->ID)
 		];
 
 
-		return $filterSort( $breadcrumbs );
+		return $filterSort($breadcrumbs);
 	}
 }
--- includes/Search.php
+++ includes/Search.php
@@ -17,17 +17,17 @@ class Search {
 	private \wpdb $wpdb;
 
 	public function __construct() {
-		$this->postTypes = [ 
-			'page'             => __( 'Pages', 'municipio-customisation' ),
-			'nyheter'          => __( 'News', 'municipio-customisation' ),
-			'jobb'             => __( 'Jobs', 'municipio-customisation' ),
-			'event'            => __( 'Events', 'municipio-customisation' ),
-			'driftinformation' => __( 'Operating information', 'municipio-customisation' )
+		$this->postTypes = [
+			'page'             => __('Pages', 'municipio-customisation'),
+			'nyheter'          => __('News', 'municipio-customisation'),
+			'jobb'             => __('Jobs', 'municipio-customisation'),
+			'event'            => __('Events', 'municipio-customisation'),
+			'driftinformation' => __('Operating information', 'municipio-customisation')
 		];
 
-		add_action( 'template_redirect', [ $this, 'setLocalVars' ] );
-		add_action( 'pre_get_posts', [ $this, 'setPostTypesToSearch' ] );
-		add_action( 'custom_search_page', [ $this, 'customSearchPage' ] );
+		add_action('template_redirect', [$this, 'setLocalVars']);
+		add_action('pre_get_posts', [$this, 'setPostTypesToSearch']);
+		add_action('custom_search_page', [$this, 'customSearchPage']);
 	}
 
 	public function setLocalVars() {
@@ -40,30 +40,30 @@ class Search {
 		$this->wpdb    = $wpdb;
 	}
 
-	public function setPostTypesToSearch( \WP_Query $query ) {
-		if ( $query->is_main_query() && $query->is_search() && ! is_admin() ) {
-			$query->set( 'post_type', array_keys( $this->postTypes ) );
-			$query->set( 'posts_per_page', 10 );
-			$query->set( 'paged', $this->getCurrentPage() );
+	public function setPostTypesToSearch(\WP_Query $query) {
+		if ($query->is_main_query() && $query->is_search() && ! is_admin()) {
+			$query->set('post_type', array_keys($this->postTypes));
+			$query->set('posts_per_page', 10);
+			$query->set('paged', $this->getCurrentPage());
 
 			$searchType = $this->getSearchType();
-			if ( $searchType !== 'all-hits' && in_array( $searchType, array_keys( $this->postTypes ) ) ) {
-				$query->set( 'post_type', $searchType );
+			if ($searchType !== 'all-hits' && in_array($searchType, array_keys($this->postTypes))) {
+				$query->set('post_type', $searchType);
 			}
 		}
 	}
 
 	public function customSearchPage() {
 		$markup           = "";
-		$componentLibrary = new ComponentLibraryInit( [] );
+		$componentLibrary = new ComponentLibraryInit([]);
 		$bladeEngine      = $componentLibrary->getEngine();
 
 		$data         = $this->getData();
 		$data['lang'] = $this->getLang();
 
 		try {
-			$markup = $bladeEngine->makeView( 'custom-search', $data, [], Plugin::VIEWPATH )->render();
-		} catch ( \Throwable $e ) {
+			$markup = $bladeEngine->makeView('custom-search', $data, [], Plugin::VIEWPATH)->render();
+		} catch (\Throwable $e) {
 			$markup .= '<pre style="border: 3px solid #f00; padding: 10px;">';
 			$markup .= '<strong>' . $e->getMessage() . '</strong>';
 			$markup .= '<hr style="background: #000; outline: none; border:none; display: block; height: 1px;"/>';
@@ -81,24 +81,24 @@ class Search {
 		$data['allHits']               = $this->getTotalHitCount();
 		$data['posts']                 = $this->getPosts();
 		$data['searchTerm']            = $this->searchTerm;
-		$data['searchTermUrl']         = urlencode( $this->searchTerm );
+		$data['searchTermUrl']         = urlencode($this->searchTerm);
 		$data['searchType']            = $this->getSearchType();
 		$data['currentPagePagination'] = $this->getCurrentPage();
-		$data['showPagination']        = \Municipio\Helper\Archive::showPagination( false, $this->wpquery );
-		$data['paginationList']        = \Municipio\Helper\Archive::getPagination( false, $this->wpquery );
+		$data['showPagination']        = \Municipio\Helper\Archive::showPagination(false, $this->wpquery);
+		$data['paginationList']        = \Municipio\Helper\Archive::getPagination(false, $this->wpquery);
 
 
 
 		$count               = $this->getResultCountByPostType();
-		$countByType         = array_map( function ($typeKey) use ($count, $data) {
-			return [ 
-				'name'    => $this->postTypes[ $typeKey ],
-				'link'    => home_url( "/?s={$data['searchTermUrl']}&type={$typeKey}" ),
+		$countByType         = array_map(function ($typeKey) use ($count, $data) {
+			return [
+				'name'    => $this->postTypes[$typeKey],
+				'link'    => home_url("/?s={$data['searchTermUrl']}&type={$typeKey}"),
 				'type_id' => $typeKey,
-				'count'   => $count[ $typeKey ] ?? 0,
+				'count'   => $count[$typeKey] ?? 0,
 				'active'  => $typeKey === $data['searchType'],
 			];
-		}, array_keys( $this->postTypes ) );
+		}, array_keys($this->postTypes));
 		$data['countByType'] = $countByType;
 
 		return $data;
@@ -109,43 +109,43 @@ class Search {
 
 		$lang = new \stdClass;
 
-		$lang->search      = __( 'Search', 'municipio' );
-		$lang->placeholder = __( 'What are you searching for?', 'municipio' );
-		$lang->hits        = _n( 'Hit', 'Hits', $this->wpquery->found_posts, 'municipio-customisation' );
-		$lang->allHits     = __( 'All hits', 'municipio-customisation' );
+		$lang->search      = __('Search', 'municipio');
+		$lang->placeholder = __('What are you searching for?', 'municipio');
+		$lang->hits        = _n('Hit', 'Hits', $this->wpquery->found_posts, 'municipio-customisation');
+		$lang->allHits     = __('All hits', 'municipio-customisation');
 		/* translators: 1 nr hits, 2 hits, 3 search string, 4 domain */
-		$lang->found = __( '%1$d %2$s for <b>"%3$s"</b> on %4$s', 'municipio-customisation' );
-		$lang->found = sprintf( $lang->found, $this->wpquery->found_posts, lcfirst( $lang->hits ), $this->searchTerm, $domain );
+		$lang->found = __('%1$d %2$s for <b>"%3$s"</b> on %4$s', 'municipio-customisation');
+		$lang->found = sprintf($lang->found, $this->wpquery->found_posts, lcfirst($lang->hits), $this->searchTerm, $domain);
 
 		return $lang;
 	}
 
 	private function getSearchType() {
-		return isset( $_GET['type'] ) ? trim( $_GET['type'] ) : 'all-hits';
+		return isset($_GET['type']) ? trim($_GET['type']) : 'all-hits';
 	}
 
 	private function getPosts() {
 		$posts = $this->wpquery->posts;
-		foreach ( $posts as $postKey => $post ) {
-			$posts[ $postKey ] = \Municipio\Helper\Post::preparePostObject( $post );
+		foreach ($posts as $postKey => $post) {
+			$posts[$postKey] = \Municipio\Helper\Post::preparePostObject($post);
 
-			$breadcrumbs = Posts::getBreadcrumbs( $post, function ($breadcrumbs) {
-				$filtered = array_filter( $breadcrumbs, function ($item) {
+			$breadcrumbs = Posts::getBreadcrumbs($post, function ($breadcrumbs) {
+				$filtered = array_filter($breadcrumbs, function ($item) {
 					return $item['title'] !== 'Sidor';
-				} );
-				return count( $filtered ) <= 1 ? [] : $filtered;
-			} );
+				});
+				return count($filtered) <= 1 ? [] : $filtered;
+			});
 
-			$posts[ $postKey ]->breadcrumbs = $breadcrumbs;
+			$posts[$postKey]->breadcrumbs = $breadcrumbs;
 		}
 
-		$posts = apply_filters( 'Municipio/Controller/Search/prepareSearchResultObject', $posts );
+		$posts = apply_filters('Municipio/Controller/Search/prepareSearchResultObject', $posts);
 
 		return $posts;
 	}
 
 	private function getTotalHitCount() {
-		$postTypes = implode( ',', array_map( fn ( $item ) => "'{$item}'", array_keys( $this->postTypes ) ) );
+		$postTypes = implode(',', array_map(fn($item) => "'{$item}'", array_keys($this->postTypes)));
 
 		$sql = "SELECT COUNT(*)
                 FROM {$this->wpdb->posts}
@@ -165,13 +165,13 @@ class Search {
 			'%' . $this->searchTerm . '%'
 		);
 
-		$count = $this->wpdb->get_var( $preparedStatement );
+		$count = $this->wpdb->get_var($preparedStatement);
 
-		return intval( $count );
+		return intval($count);
 	}
 
 	private function getResultCountByPostType() {
-		$postTypes = implode( ',', array_map( fn ( $item ) => "'{$item}'", array_keys( $this->postTypes ) ) );
+		$postTypes = implode(',', array_map(fn($item) => "'{$item}'", array_keys($this->postTypes)));
 
 		$sql = "SELECT COUNT(*) occurrences, post_type
                 FROM {$this->wpdb->posts}
@@ -192,12 +192,12 @@ class Search {
 			'%' . $this->searchTerm . '%'
 		);
 
-		$count = $this->wpdb->get_results( $preparedStatement );
+		$count = $this->wpdb->get_results($preparedStatement);
 
-		if ( is_array( $count ) && sizeof( $count ) > 0 ) {
+		if (is_array($count) && sizeof($count) > 0) {
 			$count = array_combine(
-				array_column( $count, 'post_type' ),
-				array_map( 'intval', array_column( $count, 'occurrences' ) )
+				array_column($count, 'post_type'),
+				array_map('intval', array_column($count, 'occurrences'))
 			);
 		} else {
 			$count = [];
@@ -207,10 +207,10 @@ class Search {
 	}
 
 	private function getCurrentPage() {
-		$paged = isset( $_GET['paged'] )
-			? ( intval( $_GET['paged'] ) > 0
-				? intval( $_GET['paged'] )
-				: 1 )
+		$paged = isset($_GET['paged'])
+			? (intval($_GET['paged']) > 0
+				? intval($_GET['paged'])
+				: 1)
 			: 1;
 
 		return $paged;

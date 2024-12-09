2efbbfb78481910bfd7947050b7981ab1e1ca134
Michael Claesson <michael.claesson@consid.se>
2024-12-09 14:22:53 +0100

    Page settings

---
+++ acf/php/page-settings.php
@@ -0,0 +1,61 @@
+<?php 
+
+if (function_exists('acf_add_local_field_group')) {
+    acf_add_local_field_group(array(
+    'key' => 'group_673dd0baaff48',
+    'title' => __('Page settings', 'municipio-customisation'),
+    'fields' => array(
+        0 => array(
+            'key' => 'field_673dd0bbc9739',
+            'label' => __('Theme', 'municipio-customisation'),
+            'name' => 'page_theme',
+            'aria-label' => '',
+            'type' => 'select',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'choices' => array(
+            ),
+            'default_value' => false,
+            'return_format' => 'value',
+            'multiple' => 0,
+            'allow_custom' => 0,
+            'placeholder' => '',
+            'search_placeholder' => '',
+            'allow_null' => 0,
+            'ui' => 0,
+            'ajax' => 0,
+        ),
+    ),
+    'location' => array(
+        0 => array(
+            0 => array(
+                'param' => 'post_type',
+                'operator' => '==',
+                'value' => 'page',
+            ),
+        ),
+    ),
+    'menu_order' => 0,
+    'position' => 'side',
+    'style' => 'default',
+    'label_placement' => 'left',
+    'instruction_placement' => 'label',
+    'hide_on_screen' => '',
+    'active' => true,
+    'description' => '',
+    'show_in_rest' => 0,
+    'acfe_display_title' => '',
+    'acfe_autosync' => array(
+        0 => 'json',
+    ),
+    'acfe_form' => 0,
+    'acfe_meta' => '',
+    'acfe_note' => '',
+));
+}
--- municipio-customisation.php
+++ municipio-customisation.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Alingsås Municipio customisation
 Description: Alingsås kommun's custom tweaks for Municipio
-Version: 0.1.11
+Version: 0.1.12
 Author: Consid
 Author URI: https://www.consid.se
 Textdomain: municipio-customisation
@@ -12,7 +12,7 @@ namespace AlingsasCustomisation;
 
 class Plugin {
 
-    public const VERSION = '0.1.11';
+    public const VERSION = '0.1.12';
 
     public const PATH = __DIR__;
 

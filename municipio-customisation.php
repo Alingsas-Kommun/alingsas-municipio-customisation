14eda93593e83749a28d2b1374600b2cdb6e27c8
Michael Claesson <michael.claesson@consid.se>
2025-01-20 18:13:09 +0100

    Versioning

--- municipio-customisation.php
+++ municipio-customisation.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Alingsås Municipio customisation
 Description: Alingsås kommun's custom tweaks for Municipio
-Version: 0.1.14
+Version: 0.1.15
 Author: Consid
 Author URI: https://www.consid.se
 Textdomain: municipio-customisation
@@ -12,7 +12,7 @@ namespace AlingsasCustomisation;
 
 class Plugin {
 
-    public const VERSION = '0.1.14';
+    public const VERSION = '0.1.15';
 
     public const PATH = __DIR__;
 

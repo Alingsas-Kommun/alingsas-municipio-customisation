c2c0e9b62a92a949cf2d5ab930ff05f59f154138
Michael Claesson <michael.claesson@consid.se>
2025-02-12 18:55:47 +0100

    Switch to Vite build

--- includes/Scripts.php
+++ includes/Scripts.php
@@ -7,13 +7,19 @@ use AlingsasCustomisation\Plugin;
 class Scripts {
     public function __construct() {
         add_action('wp_enqueue_scripts', function () {
-            $manifest_path = dirname(plugin_dir_path(__FILE__)) . '/dist/js/manifest.json';
+            $manifest_path = dirname(plugin_dir_path(__FILE__)) . '/dist/.vite/manifest.json';
             if (file_exists($manifest_path)) {
                 $manifest = json_decode(file_get_contents($manifest_path));
-                wp_enqueue_script('alingsas-script', dirname(plugin_dir_url(__FILE__)) . '/dist/js/' . $manifest->{'main.js'}, ['jquery'], Plugin::VERSION, ['in_footer' => true]);
-            } else {
-                wp_enqueue_script('alingsas-script', dirname(plugin_dir_url(__FILE__)) . '/dist/js/main.js', ['jquery'], Plugin::VERSION, ['in_footer' => true]);
+                wp_enqueue_script('alingsas-script', dirname(plugin_dir_url(__FILE__)) . '/dist/' . $manifest->{'src/js/main.js'}->file, ['jquery'], Plugin::VERSION, ['in_footer' => true]);
             }
         });
+
+        // Vite development
+        if (wp_get_environment_type() === 'development') {
+            add_action('wp_head', function () {
+                echo '<script type="module" src="https://localhost:5173/@vite/client"></script>';
+                echo '<script type="module" src="https://localhost:5173/src/js/main.js"></script>';
+            });
+        }
     }
 }

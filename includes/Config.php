ef3a6ee2b5562d64906c26c7ab92e001e31d8f45
Michael Claesson <michael.claesson@consid.se>
2025-03-27 09:59:30 +0100

    Turn on excerpt for pages

--- includes/Config.php
+++ includes/Config.php
@@ -6,6 +6,11 @@ use AlingsasCustomisation\Plugin;
 
 class Config {
     public function __construct() {
+        // Add post excerpt support for pages
+        add_filter('init', function() {
+            add_post_type_support('page', 'excerpt');
+        });
+
         // Remove print button
         add_filter('Municipio/Accessibility/Items', function ($items) {
             unset($items['print']);

d42530e36530c6d672f57c7580ce53ef9b4bed6e
viktor7ltz <78755786+viktor7ltz@users.noreply.github.com>
2025-09-05 14:08:25 +0200

    New implementation of 'hide title'

--- includes/ExtraSettings.php
+++ includes/ExtraSettings.php
@@ -5,61 +5,70 @@ namespace AlingsasCustomisation\Includes;
 use AlingsasCustomisation\Helpers\Appearance as AppearanceHelper;
 
 class ExtraSettings {
-    public const FIELD_BACKGROUND_STRIPE_COLOR = 'field_6718a5f939289';
+	public const FIELD_BACKGROUND_STRIPE_COLOR = 'field_6718a5f939289';
 
-    public function __construct() {
-        // General settings
-        add_filter('Modularity/Display/BeforeModule::classes', function ($classes, $args, $posttype, $ID) {
-            $background_color = get_field('background_stripe_color', $ID);
-            $no_top_margin = get_field('no_top_margin', $ID);
-            $no_bottom_margin = get_field('no_bottom_margin', $ID);
+	public function __construct() {
+		// General settings
+		add_filter('Modularity/Display/BeforeModule::classes', function ($classes, $args, $posttype, $ID) {
+			$background_color = get_field('background_stripe_color', $ID);
+			$no_top_margin = get_field('no_top_margin', $ID);
+			$no_bottom_margin = get_field('no_bottom_margin', $ID);
 
-            if (!empty($background_color) && $background_color !== 'none') {
-                $classes[] = 'has-background-stripe';
-                $classes[] = 'background-stripe-color-' . $ID;
-            }
+			if (!empty($background_color) && $background_color !== 'none') {
+				$classes[] = 'has-background-stripe';
+				$classes[] = 'background-stripe-color-' . $ID;
+			}
 
-            if ($no_top_margin) {
-                $classes[] = 'no-top-margin';
-            }
+			if ($no_top_margin) {
+				$classes[] = 'no-top-margin';
+			}
 
-            if ($no_bottom_margin) {
-                $classes[] = 'no-bottom-margin';
-            }
+			if ($no_bottom_margin) {
+				$classes[] = 'no-bottom-margin';
+			}
 
-            return $classes;
-        }, 10, 4);
-        add_filter('Modularity/Display/AfterModule', function ($html, $args, $posttype, $ID) {
-            $background_color = get_field('background_stripe_color', $ID);
+			return $classes;
+		}, 10, 4);
+		add_filter('Modularity/Display/AfterModule', function ($html, $args, $posttype, $ID) {
+			$background_color = get_field('background_stripe_color', $ID);
 
-            if (!empty($background_color) && $background_color !== 'none') {
-                $html .= '
+			if (!empty($background_color) && $background_color !== 'none') {
+				$html .= '
                 <style>
                 .background-stripe-color-' . $ID . '::before {
                     background-color: ' . AppearanceHelper::getColorValue($background_color) . ';
                 }
                 </style>
                 ';
-            }
+			}
 
-            return $html;
-        }, 10, 4);
+			return $html;
+		}, 10, 4);
 
-        add_filter('template_redirect', function () {
-            // Hide breadcrumbs if set on single page
-            if (is_singular() && get_field('hide_breadcrumbs')) {
-                add_filter('Municipio/Partials/Navigation/HelperNavBeforeContent', '__return_false');
-            }
+		add_filter('template_redirect', function () {
+			// Hide breadcrumbs if set on single page
+			if (is_singular() && get_field('hide_breadcrumbs')) {
+				add_filter('Municipio/Partials/Navigation/HelperNavBeforeContent', '__return_false');
+			}
 
-            if (is_singular() && get_field('hide_title')) {
-                add_filter('Municipio/Helper/Post/complementPostObject', function($appendFields) {
-                    $appendFields = array_filter($appendFields, function ($value) {
-                        return $value !== "post_title_filtered";
-                    });
+			if (is_singular()  && get_field('hide_title')) {
 
-                    return $appendFields;
-                });
-            }
-        });
-    }
+				// Make sure decorator class is available (in case Composer autoload not active for plugin src)
+				if (! class_exists('\\AlingsasCustomisation\\Decorators\\HideTitlePostObject')) {
+					require_once __DIR__ . '/../src/Decorators/HideTitlePostObject.php';
+				}
+
+				// Decorate the PostObject so getTitle() returns empty, hiding the H1 in templates
+				add_filter('Municipio/DecoratePostObject', function ($postObject) {
+					if (method_exists($postObject, 'getId')) {
+						$postId = $postObject->getId();
+						if ($postId && get_field('hide_title', $postId)) {
+							$postObject = new \AlingsasCustomisation\Decorators\HideTitlePostObject($postObject, true);
+						}
+					}
+					return $postObject;
+				}, 20);
+			}
+		});
+	}
 }
---
+++ includes/decorators/HideTitlePostObject.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AlingsasCustomisation\Decorators;
+
+use Municipio\PostObject\PostObjectInterface;
+use Municipio\PostObject\Decorators\AbstractPostObjectDecorator;
+
+/**
+ * HideTitlePostObject
+ *
+ * Decorator that conditionally hides the post title (returns empty string) based on a flag.
+ */
+class HideTitlePostObject extends AbstractPostObjectDecorator implements PostObjectInterface {
+	/** @var bool */
+	private bool $shouldHide;
+
+	public function __construct( PostObjectInterface $postObject, bool $shouldHide ) {
+		parent::__construct( $postObject );
+		$this->shouldHide = $shouldHide;
+	}
+
+	/**
+	 * Return empty title if we should hide it.
+	 */
+	public function getTitle() : string {
+		if ( $this->shouldHide ) {
+			return '';
+		}
+
+		return $this->postObject->getTitle();
+	}
+}

2e9cef9b74260d0bffe29e74a241564a3dec75f1
Michael Claesson <michael.claesson@consid.se>
2025-11-25 11:49:03 +0100

    Automatically add right sidebar to singular pages

---
+++ includes/Sidebar.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AlingsasCustomisation\Includes;
+
+use AcfService\Implementations\NativeAcfService;
+
+class Sidebar
+{
+    public function __construct()
+    {
+        // Make sure that right sidebar is always active on singular items
+        // unless we have specifically deactivated it
+        add_filter('is_active_sidebar', function ($isActiveSidebar, $sidebar) {
+            $acfService = new NativeAcfService();
+            $hideRightSidebar = $acfService->getField('hide_right_sidebar');
+
+            if ($hideRightSidebar && $sidebar === 'right-sidebar') {
+                return false;
+            }
+
+            if (is_singular() && $sidebar === 'right-sidebar') {
+                return true;
+            }
+            return $isActiveSidebar;
+        }, 15, 2);
+    }
+}

06a254b246c0e0ae0e13d4ee7dac98d4f5ff3bee
Michael Claesson <michael.claesson@consid.se>
2025-12-04 17:30:25 +0100

    Only add right sidebar if default template

--- includes/Sidebar.php
+++ includes/Sidebar.php
@@ -13,12 +13,13 @@ class Sidebar
         add_filter('is_active_sidebar', function ($isActiveSidebar, $sidebar) {
             $acfService = new NativeAcfService();
             $hideRightSidebar = $acfService->getField('hide_right_sidebar');
+            $pageTemplate = get_page_template_slug();
 
             if ($hideRightSidebar && $sidebar === 'right-sidebar') {
                 return false;
             }
 
-            if (is_singular() && $sidebar === 'right-sidebar') {
+            if (is_singular() && $sidebar === 'right-sidebar' && empty($pageTemplate)) {
                 return true;
             }
             return $isActiveSidebar;

3bd8a88a56a82501905da855fdd9ba39ba32ce7d
Michael Claesson <michael.claesson@consid.se>
2025-12-04 17:35:21 +0100

    Theme support for event module buttons

--- municipio-customisation.php
+++ municipio-customisation.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Alingsås Municipio customisation
 Description: Alingsås kommun's custom tweaks for Municipio
-Version: 0.2.1
+Version: 0.2.2
 Author: Consid
 Author URI: https://www.consid.se
 Text Domain: municipio-customisation
@@ -12,7 +12,7 @@ namespace AlingsasCustomisation;
 
 class Plugin {
 
-    public const VERSION = '0.2.1';
+    public const VERSION = '0.2.2';
 
     public const PATH = __DIR__;
 

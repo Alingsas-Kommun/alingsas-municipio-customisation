3ce20f442a724bf62dda7f6e5c04f0cbec07f089
Viktor Schultz <viktor.schultz@consid.se>
2026-02-06 10:26:22 +0100

    override A-E-M-I event search params

--- includes/Event.php
+++ includes/Event.php
@@ -7,6 +7,30 @@ class Event extends \EventManagerIntegration\PostTypes\Events {
 
 	public function __construct() {
 		add_filter( 'Municipio/Template/event/single/viewData', [ $this, 'singleViewData' ], 5, 1 );
+		add_filter( 'Municipio/viewData', [ $this, 'modifyEventArchiveUrl' ], 20, 1 );
+	}
+
+	/**
+	 * Modify the "Show all occasions" link URL
+	 * Runs after api-event-manager-integration sets eventArchive 
+	 *
+	 * @param array $data View data
+	 * @return array Modified view data
+	 */
+	public function modifyEventArchiveUrl( $data ) {
+		if ( ! is_singular( 'event' ) || empty( $data['event']['eventArchive'] ) ) {
+			return $data;
+		}
+
+		global $post;
+
+		$data['event']['eventArchive'] = add_query_arg(
+			'archive_search',
+			urlencode( $post->post_title ),
+			get_post_type_archive_link( parent::$postTypeSlug )
+		);
+
+		return $data;
 	}
 
 	public function singleViewData( $data ) {

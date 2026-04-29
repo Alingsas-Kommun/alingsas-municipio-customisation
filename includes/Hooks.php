85c2963b14db891bcfcd3a088722bc2a6a3924f1
Viktor Schultz <viktor.schultz@consid.se>
2026-04-29 11:37:13 +0200

    More info rendered on job ads

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -5,10 +5,8 @@ namespace AlingsasCustomisation\Includes;
 /**
  * Miscellaneous WordPress hooks.
  */
-class Hooks
-{
-    public function __construct()
-    {
+class Hooks {
+    public function __construct() {
         add_action('admin_notices', function () {
             $screen = get_current_screen();
             if (!$screen || $screen->post_type !== 'anslagstavla' || $screen->base !== 'post') {
@@ -23,7 +21,7 @@ class Hooks
             </div>';
         });
 
-        add_filter('Municipio/Template/lediga-jobb/single/viewData', [$this, 'appendJobWorkHoursToInformationList'], 10, 1);
+        add_filter('Municipio/Template/lediga-jobb/single/viewData', [$this, 'appendExtraDataToJobPosting'], 10, 1);
     }
 
     /**
@@ -32,12 +30,25 @@ class Hooks
      * @param array<string, mixed> $viewData Municipio single view data.
      * @return array<string, mixed>
      */
-    public function appendJobWorkHoursToInformationList(array $viewData): array
-    {
+    public function appendExtraDataToJobPosting(array $viewData): array {
         $post = $viewData['post'] ?? null;
         if (!is_object($post) || !method_exists($post, 'getSchemaProperty')) {
             return $viewData;
         }
+
+        $readMoreUrl = $post->getSchemaProperty('readMoreUrl');
+        if ($readMoreUrl !== null && $readMoreUrl !== '') {
+            $viewData['readMoreUrl'] = $readMoreUrl;
+        }
+
+        $jobStartDate = $post->getSchemaProperty('jobStartDate');
+        if ($jobStartDate !== null && $jobStartDate !== '') {
+            $viewData['informationList'][] = [
+                'label' => __('Anställningsstart', 'municipio-customisation'),
+                'value' => is_scalar($jobStartDate) ? (string) $jobStartDate : wp_json_encode($jobStartDate),
+            ];
+        }
+
 
         $workHours = $post->getSchemaProperty('workHours');
         if ($workHours !== null && $workHours !== '') {

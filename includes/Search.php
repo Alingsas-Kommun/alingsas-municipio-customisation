e851f3e0909251dd54b409946e685bade5457d69
Michael Claesson <michael.claesson@consid.se>
2026-06-11 22:32:41 +0200

    Change which lang variable to modify

--- includes/Search.php
+++ includes/Search.php
@@ -6,7 +6,7 @@ class Search {
     public function __construct() {
         add_filter( 'Municipio/viewData', function ( $data ) {
             if ( is_search() ) {
-                $data['lang']->search = $data['lang']->searchResults;
+                $data['lang']->searchTitle = $data['lang']->searchResults;
             }
             return $data;
         } );
